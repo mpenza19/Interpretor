ff0780985e40ed0b1923380e446b4ac550c8c31b
Matthew Penza <mpenza@cs.princeton.edu>
2019-01-07 11:46:21 -0500

    cleanup

---
+++ src/clean_dir.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+rm -r __pycache__ text parses latmor_files gen 2> cleanup_log.txt
+mkdir text parses parses/source parses/target latmor_files gen 2>> cleanup_log.txt
+rm $2 *.pyc gen_errors.txt 2>> cleanup_log.txt
+rm cleanup_log.txt
--- src/translate.sh
+++ src/translate.sh
@@ -1,14 +1,11 @@
-#!/bin/sh
+    #!/bin/sh
 
 # Usage:
 # bash scanlat.sh input_text_file is_macronized_input
 #
 # where input_text_file is in the subdirectory ../input
 
-rm -r text parses latmor_files gen 2> cleanup_log.txt
-mkdir text parses parses/source parses/target latmor_files gen 2>> cleanup_log.txt
-rm $2 *.pyc gen_errors.txt 2>> cleanup_log.txt
-rm cleanup_log.txt
+bash clean_dir.sh
 
 echo $1
 echo $2
